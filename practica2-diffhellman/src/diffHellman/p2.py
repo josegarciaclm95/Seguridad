bb12b7ef43a006a48d7a846fab6132da9cc6e42f
Jose Maria <josemariagarcia95@gmail.com>
2016-11-08 21:16:05 +0100

    Optimizacion en algoritmo

--- practica2-diffhellman/src/diffHellman/p2.py
+++ practica2-diffhellman/src/diffHellman/p2.py
@@ -1,5 +1,9 @@
+from multiprocessing import Process
+
 def ExpM (n,x,p):
-    b = bin(x)[2:]
+    #b = bin(x)[2:]
+    b ="{0:b}".format(x)
+
 
     final = 1
     lenb = len(b) - 1
@@ -13,30 +17,23 @@ def ExpM (n,x,p):
             final *= n
     return (final % p)
 
-def ataque (n,p, alice, bob):
-    i = 0
-    z = None
-    flag = 2
-    result = [-1,-1]
-    while flag != 0:
+def ataque (n,p, alice, bob, i,j):
+    while i != j :
         i += 1
         deco = ExpM(n,i,p)
         if deco == alice:
             print('x de Alice: %i' % i)
-            result[0] = i
-            flag -=1
         if deco == bob:
             print('x de Bob: %i' % i)
-            flag -=1
-            result[1] = i
-    return result
 
 # Ejemplo https://en.wikipedia.org/wiki/Diffie%E2%80%93Hellman_key_exchange
 
-n = 65537#5
-p = 1372933 #23
-xa = 5858999999#6
-xb = 7888899999 #15
+n = int(7245627542842336859)  #65537 #5
+p = int(3523269789483225643)  #1372933 #23
+
+'''
+xa = 2930009534032416290
+xb = 6290936979721568605
 
 alice =  ExpM(n,xa,p) # 8
 print('Alice: %i' % alice)
@@ -49,13 +46,25 @@ print('Alice s: %i' % alice_s)
 
 bob_s = ExpM(alice,xb,p)
 print('Bob s: %i' % bob_s)
+'''
 
-print('---Ataque---')
-sDescifrada = ataque(n,p,alice,bob)
-print('Claves Alice-Bob:' + str(sDescifrada))
-print('---Comprobacion de Ataque---')
-aliceAgain = ExpM(n,sDescifrada[0],p)
-bobAgain = ExpM(n,sDescifrada[1],p)
-print('Alice: %i' % aliceAgain)
-print('Bob: %i' % bobAgain)
-#print('Clave descifrada: %i' % sDescifrada)
+alice = int(2930009534032416290)#6
+bob = int(6290936979721568605) #15 # Bob el más grande siempre.
+
+
+def espacio(number):
+    number *= 10000000
+    while True:
+        print('---Ataque de %i a %i ---' % (number, number + 10000000))
+        ataque(n,p,alice,bob,number,number + 10000000)
+        number += 40000000
+
+p_list = []
+
+if __name__ == '__main__':
+    for number in range(4):
+        p = Process(target=espacio, args=(number,))
+        p_list.append(p)
+
+for p in p_list:
+    p.start()
