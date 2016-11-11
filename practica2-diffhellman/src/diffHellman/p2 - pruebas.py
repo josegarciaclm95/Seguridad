dd573c74e2b9c0564a8957e69b0b9f90ff9cf8a2
Jose Maria <josemariagarcia95@gmail.com>
2016-11-11 12:24:19 +0100

    Pruebas de Diffie

--- practica2-diffhellman/src/diffHellman/p2 - pruebas.py
+++ practica2-diffhellman/src/diffHellman/p2 - pruebas.py
@@ -28,15 +28,13 @@ def ataque (n,p, alice, bob, i,j):
 
 # Ejemplo https://en.wikipedia.org/wiki/Diffie%E2%80%93Hellman_key_exchange
 
-
-n = int(7245627542842337021864103589)  #65537 #5
-p = int(9412350003523269789483225961)  #1372933 #23
-xjose = int(2115478207654128809)
+n = int(33)  #65537 #5
+p = int(1999)  #1372933 #23
+xjose = int(6041)
 resJose = ExpM(n,xjose,p)
 print('Resultado Jose: %i' % resJose)
 
-
-xdani = int(7113897121456987223)
+xdani = int(6111)
 resDani = ExpM(n,xdani,p)
 print('Resultado Dani: %i' % resDani)
 
@@ -45,20 +43,3 @@ print('Resultado secreto Jose: %i' % resJose_s)
 
 resDani_s = ExpM(resJose,xdani,p)
 print('Resultado secreto Dani: %i' % resDani_s)
-
-def espacio(number):
-    number *= 10000000
-    while True:
-        print('---Ataque de %i a %i ---' % (number, number + 10000000))
-        ataque(n,p,resJose,resDani,number,number + 10000000)
-        number += 40000000
-
-p_list = []
-
-if __name__ == '__main__':
-    for number in range(4):
-        p = Process(target=espacio, args=(number,))
-        p_list.append(p)
-
-for p in p_list:
-    p.start()
